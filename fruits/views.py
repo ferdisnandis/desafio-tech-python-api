83ce2343e02040eaac192911ea4d1206458594f1
Fernanda <fe97964@gmail.com>
2022-03-30 01:29:29 -0300

    Delete e Update feito

--- fruits/views.py
+++ fruits/views.py
@@ -1,3 +1,4 @@
+from urllib import request
 from django.shortcuts import redirect, render
 from fruits.forms import FruitsForm
 from fruits.models import Fruits
@@ -23,3 +24,22 @@ def view(request, pk):
     data = {}
     data['db'] = Fruits.objects.get(pk=pk)
     return render(request, 'view.html', data)
+
+def update(request, pk):
+    data = {}
+    data['db'] = Fruits.objects.get(pk=pk)
+    form = FruitsForm(request.POST or None, instance=data['db'])
+    if form.is_valid():
+        form.save()
+    return redirect('home')
+
+def edit(request, pk):
+    data = {}
+    data['db'] = Fruits.objects.get(pk=pk)
+    data['form'] = FruitsForm(instance=data['db'])
+    return render(request, 'form.html', data)
+
+def delete(request, pk):
+    db = Fruits.objects.get(pk=pk)
+    db.delete()
+    return redirect('home')
